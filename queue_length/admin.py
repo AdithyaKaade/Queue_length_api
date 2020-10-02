6932504b7f168fa6f530b28b98525034d3c0b1df
AdithyaKaade <adithya.kaade23@gmail.com>
2020-10-02 17:45:18 +0530

    Resgister admin

--- queue_length/admin.py
+++ queue_length/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
-
+from .models import Logdata_put
 # Register your models here.
+class LogDataAdmin(admin.ModelAdmin):
+    pass
+admin.site.register(Logdata_put, LogDataAdmin)
